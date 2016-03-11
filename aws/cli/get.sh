457195a5d808b9c2ba5ab3d107b882cd459512d5
Bijan Hoomand <hoomand@gmail.com>
2016-03-11 14:59:19 +1100

    Add aws cli helper set of commands

---
+++ aws/cli/get.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+usage () {
+	echo "$0 runningInstances|help"
+	echo "This script is meant to be a quick cheat sheet for aws cli to run most useful commands by just passing the right argument to it. It doesn't support changing profiles and other options for now, but I might improve it later :)"
+	exit 1
+}
+
+if [[ $# != 1 ]]
+then
+	usage
+fi
+
+case "$1" in
+	"runningInstances")
+		aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,PublicDnsName]' --filters Name=instance-state-name,Values=running --output text
+		;;
+	*)
+		usage
+		;;
+esac
+
